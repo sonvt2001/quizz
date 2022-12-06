f0ebb82b4f35620cf436e39a48e5c2430082fc14
4501104200 <113413905+sonvt2001@users.noreply.github.com>
2022-12-06 16:29:07 +0700

    quizz

---
+++ database/config.php
@@ -0,0 +1,9 @@
+<?php
+//database configurations
+$host ="localhost";
+    $username="root";
+    $password ="";
+    $dbname ="quiz";
+
+    $conn = new mysqli($host, $username, $password, $dbname);
+?>
